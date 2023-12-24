9efb471a7d9e932926ac280f3e2eb981da844ad9
laondev5 <ekohabraham5@gmail.com>
2023-12-24 04:08:20 +0100

    completed navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,7 @@ import {
   PhoneIcon,
 } from "@heroicons/react/16/solid";
 import { Popover, Dialog, Disclosure, Transition } from "@headlessui/react";
+import { cn } from "../lib/utils";
 
 const products = [
   {
@@ -152,6 +153,106 @@ const Header = () => {
           </a>
         </div>
       </nav>
+
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 w-full z-10 overflow-y-auto bg-blue-700 p-6 sm:max-w-sm ring-1 ring-gray-800/5">
+          <div className="flex items-center justify-between">
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">BUKER.COM</span>
+              <span className="text-[16px] font-bold cursor-pointer text-white md:hidden">
+                BUKER.COM
+              </span>
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 rounded-md text-white"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+
+          <div className="mt-6 flow-root">
+            <div className="-m-6 divide-y divide-y-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex items-center justify-between w-full rounded-lg py-2 pl-3 pr-3.5 text-white font-semibold hover:bg-blue-900 text-base leading-7">
+                        <span className="sr-only">Open</span>
+                        <span className="block px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+                          Stay
+                        </span>
+                        <ChevronDownIcon
+                          className={cn(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callToAction].map((item) => (
+                          <Disclosure.Button
+                            as="a"
+                            key={item.name}
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-10 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <div className="">
+                  <a
+                    href="#"
+                    className="text-sm text-white block leading-6 font-semibold hover:bg-blue-800 p-3"
+                  >
+                    Flight
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm text-white block leading-6 font-semibold hover:bg-blue-800 p-3"
+                  >
+                    Car Renals
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm text-white block leading-6 font-semibold hover:bg-blue-800 p-3"
+                  >
+                    Attractions
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm text-white block leading-6 font-semibold hover:bg-blue-800 p-3"
+                  >
+                    Flight + Hotels
+                  </a>
+                </div>
+
+                <div className="block hover:bg-blue-800 p-3 mt-6">
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-white"
+                  >
+                    Login <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 };
